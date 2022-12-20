6df6e3ccec143042beb6caa6a27c515e4aba8b45
YoonSung Kim <opqudk@gmail.com>
2022-12-20 10:00:53 +0900

    Initial commit

---
+++ src/main/java/com/la/mollaapi/MollaApiApplication.java
@@ -0,0 +1,13 @@
+package com.la.mollaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MollaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MollaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/la/mollaapi/MollaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.la.mollaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MollaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
